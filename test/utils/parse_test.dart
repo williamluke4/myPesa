8b12000cb305ee97d1ce4b6fcc27a2128c73a2a7
James N <jhnichol@gmail.com>
2022-04-20 12:57:33 +0300

    initial

--- test/utils/parse_test.dart
+++ test/utils/parse_test.dart
@@ -9,6 +9,12 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:my_pesa/data/models/transaction.dart';
 import 'package:my_pesa/utils/parse.dart';
 
+/*
+
+*/
+
+String depositTransaction = '''PK95FQHDN1 Confirmed. On 9/11/21 at 7:07 PM Give Ksh60,000.00 cash to Naivas Supermarket Kilifi New M-PESA balance is Ksh104,061.96. You can now access M-PESA via *334#''';
+
 String sentTransaction =
     '''QC352PDIVL Confirmed. Ksh1,000.00 sent to SOPHIA  MWENI 0702972812 on 3/3/22 at 5:28 PM. New M-PESA balance is Ksh25,203.19. Transaction cost, Ksh12.00. Amount you can transact within the day is 292,600.00. Send KES100 & below to POCHI LA BIASHARA for FREE! To reverse, foward this message to 456.''';
 String sentTransactionWrongTimeFormat =
@@ -40,6 +46,16 @@ void main() {
 
       expect(transaction.recipient, '0780603578');
       expect(transaction.dateTime, DateTime(2021, 1, 16, 17, 9));
+    });
+    test('depositTransaction', () {
+      final transaction = parseMpesaTransaction(depositTransaction) as Transaction;
+      expect(transaction.amount, equals('1,000.00'));
+      expect(transaction.balance, equals('25,203.19'));
+      expect(transaction.txCost, equals('12.00'));
+      expect(transaction.type, equals(TransactionType.OUT));
+
+      expect(transaction.recipient, 'SOPHIA  MWENI 0702972812');
+      expect(transaction.dateTime, DateTime(2022, 3, 3, 17, 28));
     });
 
     // TODO(williamluke4): Add support for reversed transactions.
